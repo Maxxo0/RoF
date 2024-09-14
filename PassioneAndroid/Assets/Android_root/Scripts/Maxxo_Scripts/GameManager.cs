24b163773f572e3b06ac78d2635dd255194be4b9
Maxxo0 <maximoamorebonapasta12@gmail.com>
2024-09-14 12:47:21 +0200

    Inicio Scripts

---
+++ PassioneAndroid/Assets/Android_root/Scripts/Maxxo_Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Character character;
+    public List<Card> playerDeck = new List<Card>();
+    public List<Card> cardLibrary = new List<Card>();
+    public int floorNumber = 1;
+    public int goldAmount;
+    //PlayerStatsUI playerStatsUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
